8ab432de89d83ea511e0da5235afb42b2f8e8014
muscabinhoyare@gmail.com <muscab-11>
2023-10-28 11:03:59 +0300

    Here is the exercise

--- challenge.js
+++ challenge.js
@@ -1,2 +1,28 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function calculator(num1, num2, operator) {
+    let answer;
+    
+    if (operator === "+") {
+        answer = num1 + num2;
+    } else if (operator === "-") {
+        answer = num1 - num2;
+    } else if (operator === "*") {
+        answer = num1 * num2;
+    } else if (operator === "/") {
+        answer = num1 / num2;
+    } else {
+      console.log("Invalid operator.");
+      return;
+    }
+    
+    console.log("Result: " + answer);
+  }
+  
+  // Example usage
+  calculator(5, 2, "+"); // Output: Result: 7
+  calculator(5, 2, "-"); // Output: Result: 3
+  calculator(5, 2, "*"); // Output: Result: 10
+  calculator(5, 2, "/"); // Output: Result: 2.5
+  calculator(5, 2, "%"); // Output: Invalid operator.
